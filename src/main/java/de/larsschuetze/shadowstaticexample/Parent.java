723f9f7f6f7155eaa461576e54c9598daafb6af2
Lars Schuetze <dev@larsschuetze.de>
2015-08-20 12:59:38 +0200

    Move files to the root folder

---
+++ src/main/java/de/larsschuetze/shadowstaticexample/Parent.java
@@ -0,0 +1,12 @@
+package de.larsschuetze.shadowstaticexample;
+
+public class Parent {
+	
+	public static void StaticMethod() {
+		System.out.println("Static method from Parent");
+	}
+	
+	public void method() {
+		System.out.println("Normal method from Parent");
+	}
+}
